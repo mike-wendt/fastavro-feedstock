d00a7315cd66f3ac44f99b7451814283ce47d999
Miki Tebeka <miki.tebeka@gmail.com>
2017-04-19 08:02:42 +0300

    0.12.2 and more tests

--- recipe/test_recipe.py
+++ recipe/test_recipe.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """Test that meta.yaml is a valid YAML after being processed by Jinja"""
 
+from hashlib import sha256
 from io import StringIO
-from pprint import pprint
+from urllib.request import urlopen
 
 from jinja2 import Template
 import yaml
@@ -11,4 +12,11 @@ with open('meta.yaml') as fp:
     t = Template(fp.read())
 
 io = StringIO(t.render(()))
-pprint(yaml.load(io))
+data = yaml.load(io)  # Will fail on invalid YAML
+
+m = sha256()
+with urlopen(data['source']['url']) as fp:
+    m.update(fp.read())
+
+if m.hexdigest() != data['source']['sha256']:
+    print('bad digest')
